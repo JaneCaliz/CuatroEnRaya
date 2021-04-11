74123de96036275a7e03fa52dd4ec2db7da2ab60
Jane <calizjane@gmail.com>
2021-04-11 18:47:16 +0200

    controller

--- src/controladores/IniciarSesionController.java
+++ src/controladores/IniciarSesionController.java
@@ -10,10 +10,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -22,6 +29,17 @@ import javafx.stage.Stage;
  * @author ydavpacat
  */
 public class IniciarSesionController implements Initializable {
+
+    @FXML
+    private TextField usuario;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private CheckBox show;
+    @FXML
+    private Hyperlink recuerdame;
+    @FXML
+    private Button iniciar;
 
     /**
      * Initializes the controller class.
@@ -54,6 +72,14 @@ public class IniciarSesionController implements Initializable {
 //        }
 //        
         
+    }
+
+    @FXML
+    private void check(ActionEvent event) {
+    }
+
+    @FXML
+    private void inicio(ActionEvent event) {
     }
     
 }
