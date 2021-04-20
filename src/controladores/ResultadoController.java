236fe57ba60976066f31b9aa2b133974c7f8f5b0
Jane <calizjane@gmail.com>
2021-04-20 12:12:59 +0200

    Resultado

---
+++ src/controladores/ResultadoController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class ResultadoController implements Initializable {
+
+    @FXML
+    private Text resultado;
+    @FXML
+    private Text ganador;
+    @FXML
+    private Text point;
+    @FXML
+    private Button salir;
+    @FXML
+    private Button replay;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
