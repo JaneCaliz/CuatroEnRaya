262c162e469a8be2b32694523047175a2a725992
Jane <calizjane@gmail.com>
2021-05-06 21:47:45 +0200

    mensajes error

--- src/controladores/RegistrarseController.java
+++ src/controladores/RegistrarseController.java
@@ -1,7 +1,9 @@
 package controladores;
 
+import DBAccess.Connect4DAOException;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,10 +15,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.Connect4;
+import model.Player;
 
 public class RegistrarseController implements Initializable {
 
@@ -64,6 +69,10 @@ public class RegistrarseController implements Initializable {
     private ImageView iavatard;
     @FXML
     private ImageView iavatar3;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private PasswordField cpassword;
 
 
     @Override
@@ -135,6 +144,55 @@ public class RegistrarseController implements Initializable {
 
     @FXML
     private void registrarse(ActionEvent event) {
+        try {
+            Connect4 BD = Connect4.getSingletonConnect4();
+            if(!Player.checkNickName(usuario.getText()) || BD.exitsNickName(usuario.getText())){
+                if(!Player.checkNickName(usuario.getText())){
+                    eusuario.setText("Caracter inválido. Símbolos válidos: -, _ y alfanuméricos");
+                }
+                if(usuario.getText().length() < 6 || usuario.getText().length() > 15){
+                    eusuario.setText("El usuario debe tener entre 6 a 15 caracteres");
+                }
+                if(BD.exitsNickName(usuario.getText())){
+                    eusuario.setText("Usuario ya existente");
+                }
+            }
+            else{
+                eusuario.setText("");
+            }
+            if(!Player.checkEmail(email.getText())){
+                eemail.setText("Por favor introduzca un correo existente");
+            }
+            else{
+                eemail.setText("");
+            }
+            String pass = password.getText();
+            if(!Player.checkPassword(password.getText())){
+                if(!Player.checkPassword(password.getText())){
+                    epassword.setText("La contraseña debe contener: una mayúscula, una minúscula, un dígito, un carácter espacial (!@#$%&*()-+=) y ningún espacio en blanco");
+                }
+                if(password.getText().length() < 8 || password.getText().length() > 20){
+                    epassword.setText("La contraseña debe tener entre 6 a 15 caracteres");
+                }
+            }
+            else{
+                epassword.setText("");
+            }
+            if(!pass.equals(password.getText()) && Player.checkPassword(password.getText())){
+                ecpassword.setText("Las contraseñas no coinciden");
+            }
+            else{
+                ecpassword.setText("");
+            }
+            if(edad.getValue().toEpochDay() < LocalDate.now().minusYears(12).toEpochDay()){
+                eage.setText("Debes ser mayor de 12 años para jugar");
+            }
+            else{
+                eage.setText("");
+            }
+        } catch (Connect4DAOException ex) {
+            Logger.getLogger(RegistrarseController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
