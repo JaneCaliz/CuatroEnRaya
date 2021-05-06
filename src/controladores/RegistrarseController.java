42aa1dced1468848351d7f955f7006d1b35c261b
Jane <calizjane@gmail.com>
2021-05-06 19:39:38 +0200

    registrarse

--- src/controladores/RegistrarseController.java
+++ src/controladores/RegistrarseController.java
@@ -7,14 +7,51 @@ package controladores;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
-/**
- * FXML Controller class
- *
- * @author Alicia
- */
 public class RegistrarseController implements Initializable {
+
+    @FXML
+    private TextField usuario;
+    @FXML
+    private Text eusuario;
+    @FXML
+    private TextField email;
+    @FXML
+    private Text eemail;
+    @FXML
+    private Text epassword;
+    @FXML
+    private Text ecpassword;
+    @FXML
+    private DatePicker edad;
+    @FXML
+    private Text eage;
+    @FXML
+    private ImageView avatar;
+    @FXML
+    private Button avatar1;
+    @FXML
+    private Button avatar4;
+    @FXML
+    private Button avatar2;
+    @FXML
+    private Button avatard;
+    @FXML
+    private Button avatar3;
+    @FXML
+    private Button upload;
+    @FXML
+    private Button cancelar;
+    @FXML
+    private Button registrarse;
 
     /**
      * Initializes the controller class.
@@ -23,5 +60,37 @@ public class RegistrarseController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void avatar1(ActionEvent event) {
+    }
+
+    @FXML
+    private void avatar4(ActionEvent event) {
+    }
+
+    @FXML
+    private void avatar2(ActionEvent event) {
+    }
+
+    @FXML
+    private void avatard(ActionEvent event) {
+    }
+
+    @FXML
+    private void avatar3(ActionEvent event) {
+    }
+
+    @FXML
+    private void upload(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void registrarse(ActionEvent event) {
+    }
     
 }
